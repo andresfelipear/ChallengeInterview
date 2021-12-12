03975fc2214630c555fa75c4513e725aa46ce81c
Andres Arevalo <andresfelipear@gmail.com>
2021-12-12 13:46:55 -0800

    solution challengeA, test ok

--- src/challenges/challengeA/ChallengeA.jsx
+++ src/challenges/challengeA/ChallengeA.jsx
@@ -1,13 +1,40 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 
 
 const ChallengeA = () => {
     const [list, setList] = useState([]);
     const [inputText, setInputText] = useState("")
 
+    const handleChange = (e) => {
+        setInputText(e.target.value)
+
+    }
+
+    const handleSubmit = () => {
+        if (inputText != "") {
+            setList([...list, inputText])
+            setInputText("")
+        }
+    }
+
+    const handleClick = (item) => {
+        setList([...list.filter(e => e !== item)])
+    }
+
     return (
         <div>
-            
+            <input type="text" value={inputText} onChange={handleChange} />
+            <br /> <br />
+            <button onClick={handleSubmit}> Add </button>
+            <ul>
+                {
+                    list.length > 0 && list.map((item, index) => {
+                        return (
+                            <li key={index} onClick={() => handleClick(item)}>{item} </li>
+                        )
+                    })
+                }
+            </ul>
         </div>
     )
 }
