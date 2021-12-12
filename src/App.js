3a8e74399f7cc896e348c7c9d1a8f658715827d2
Andres Arevalo <andresfelipear@gmail.com>
2021-12-12 13:27:16 -0800

    doing some basics v2

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import ChallengeA from './challenges/challengeA/ChallengeA';
 
--- src/challenges/challengeA/ChallengeA.jsx
+++ src/challenges/challengeA/ChallengeA.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const ChallengeA = () => {
     return (
         <div>
-            
+            test
         </div>
     )
 }
