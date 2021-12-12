a9905a0644777481019fd22dc532320978487bba
Andres Arevalo <andresfelipear@gmail.com>
2021-12-12 14:52:52 -0800

    save data and show in the ui

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import ChallengeA from './challenges/challengeA/ChallengeA';
+import ChallengeB from './challenges/challengeB/ChallengeB';
 
 function App() {
 
   
   return (
     <div className="App">
-      <ChallengeA/>
+      {/* <ChallengeA/> */}
+      <ChallengeB/>
     </div>
   );
 }
--- src/challenges/challengeB/ChallengeB.jsx
+++ src/challenges/challengeB/ChallengeB.jsx
@@ -1,25 +1,36 @@
-import React,{useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 
 
 function ChallengeB() {
 
-    const [todos,setTodos] = useState([{
-        userId:'',
-        id:'',
-        title:'',
-        completed:'' 
-    }])
+    const [todos, setTodos] = useState([])
 
     const url = "http://jsonplaceholder.typicode.com/todos";
-    fetch(url).then(res=>res.json()).then(response =>{
-        console.log(response);
-        // setTodos({
-        //     userId: response
-        // })
-    })
+
+    useEffect(() => {
+        fetch(url).then(res => res.json()).then(response => {
+            setTodos(response);
+        })
+    }, [])
+
     return (
         <div>
-            
+            <ul>
+                {
+                    todos.map((todo, key) => {
+                        return (
+                            <li key={key}>
+                               userId: {todo.userId} <br/>
+                               Id: {todo.id} <br/>
+                               title: {todo.title} <br/>
+                               completed: {todo.completed} <br/> <br/>
+                            </li>
+                            
+                        )
+                    })
+                }
+            </ul>
+
         </div>
     )
 }
